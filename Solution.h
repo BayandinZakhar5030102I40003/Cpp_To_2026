32aba27c1c0e0f0f1beaf84c0b9f1975d3c9b19e
BayandinZakhar5030102I40003 <bayandin.za@edu.spbstu.ru>
2026-04-15 23:31:59 +0300

    Corrections_Add_classes

---
+++ Solution.h
@@ -0,0 +1,15 @@
+#ifndef SOLUTION_H
+#define SOLUTION_H
+
+class Solution {
+ public:
+  int rootsCount;
+  double x1, x2;
+
+  Solution(int count = 0, double r1 = 0, double r2 = 0)
+      : rootsCount(count), x1(r1), x2(r2) {}
+
+  ~Solution() {}
+};
+
+#endif
